16e466d1b3d0dc3dedcfb22944e8760591b209c3
Mohamedalh143 <110286469+Mohammed2006bh@users.noreply.github.com>
2026-02-25 15:07:35 +0300

    proti v8

--- app/components/MobileBlocker.tsx
+++ app/components/MobileBlocker.tsx
@@ -163,6 +163,27 @@ export default function MobileBlocker() {
                 Mohamed Device Signature Detected ✔
               </div>
 
+            
+             {/* Device Info */}
+             <div className="text-xs text-green-400 mb-6 text-left space-y-1 border border-green-500/30 p-3 rounded-lg bg-black/40">
+               <p className="font-semibold text-green-300">
+                 {matchResult?.device?.name}
+               </p>
+
+              <p>
+                 Width: <span className="text-green-200">{deviceInfo?.width}px</span>
+               </p>
+
+               <p>
+                 DPR: <span className="text-green-200">{deviceInfo?.dpr}</span>
+               </p>
+
+               <p>
+                 Aspect Ratio:{" "}
+                 <span className="text-green-200">{deviceInfo?.aspect}</span>
+               </p>
+            </div>
+
               <button
                 onClick={() => setConfirmed(true)}
                 className="border border-green-500 px-4 py-2 rounded-lg 
